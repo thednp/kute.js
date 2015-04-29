387b0c6783a419668c3c60f9373e5e9a5922cf4a
Shavkatov Davlat Talatovich <dalisoft@mail.ru>
2015-04-29 17:40:09 +0500

    Queue/Chaining implement
    jQuery.animate() like chained animations for kute.js jQuery plug-in

--- dist/kute.jquery.js
+++ dist/kute.jquery.js
@@ -3,8 +3,15 @@
 
 (function($) {
 	$.fn.Kute = function( options ) {
-		return this.each(function(){
+	function fn(next){
+		if ( next ) {
+			options.finish = options.finish ? function () {
+				options.finish();
+				next();
+			} : next;
+		}
 			new KUTE.Animate( this, options );
-		});
+	}
+		return $.queue ? this.queue(fn) : this.each(fn);
 	};	
 })(jQuery);
