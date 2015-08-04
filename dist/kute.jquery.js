15df42234cf6fd50deb59401c1e4dc9e5707eef6
thednp <thednp@users.noreply.github.com>
2015-08-04 05:56:16 +0300

    Update kute.jquery.js

--- dist/kute.jquery.js
+++ dist/kute.jquery.js
@@ -1,10 +1,18 @@
 // KUTE jQuery Plugin for kute.js | by dnp_theme | License - MIT
 // $('selector').Kute(method, options);
 
-(function($) {
-	$.fn.Kute = function( method, options ) { // method can be Animate(), fromTo(), to(), stop(), start(), chain(), etc
-		return this.each(function(){
-			new KUTE[method]( this, options );
-		});
-	};	
-})(jQuery);
+(function($) {		
+	$.fn.KUTE = function( method, start, end, ops ) { // method can be Animate(), fromTo(), to(), stop(), start(), chain(), pause()	
+		var mt = this[0][method], i, l = this.length;
+		if ( typeof mt === 'function' ) {
+			mt.apply(this[0]);
+		}
+		for (i=0;i<l;i++){
+			if ( method === 'to' ) {				
+				return new KUTE[method]( this[i], start, end ); // here start is end and end is ops 
+			} else if ( method === 'fromTo' || method === 'Animate' ) {
+				return new KUTE[method]( this[i], start, end, ops );
+			}			
+		}
+	};
+})(jQuery);		
