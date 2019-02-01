7105d48d41c55a071dade19e72df30dbe774ae93
thednp <thednp@users.noreply.github.com>
2019-02-01 12:04:45 +0200

    Changes:
    * Removed jQuery plugin, moved into experiments branch
    * Updated demo pages
    * Updated build script
    * Updated dist files

--- build.js
+++ build.js
@@ -18,7 +18,6 @@ function replaceK(s) {
     if (/attr/.test(s)) { return 'Attributes Plugin'}
     if (/svg/.test(s)) { return 'SVG Plugin'}
     if (/css/.test(s)) { return 'CSS Plugin'}
-    if (/jq/.test(s)) { return 'jQuery Plugin'}
     if (/text/.test(s)) { return 'Text Plugin'}
   } else {
     return 'Core Engine';
@@ -40,7 +39,6 @@ function handleError(err) {
 // Minify files
 minify('kute-attr.js', 'dist/kute-attr.min.js');
 minify('kute-css.js', 'dist/kute-css.min.js');
-minify('kute-jquery.js', 'dist/kute-jquery.min.js');
 minify('kute-svg.js', 'dist/kute-svg.min.js');
 minify('kute-text.js', 'dist/kute-text.min.js');
 minify('kute.js', 'dist/kute.min.js');
@@ -48,7 +46,6 @@ minify('kute.js', 'dist/kute.min.js');
 // Now also to the demo
 minify('kute-attr.js', 'demo/src/kute-attr.min.js');
 minify('kute-css.js', 'demo/src/kute-css.min.js');
-minify('kute-jquery.js', 'demo/src/kute-jquery.min.js');
 minify('kute-svg.js', 'demo/src/kute-svg.min.js');
 minify('kute-text.js', 'demo/src/kute-text.min.js');
 minify('kute.js', 'demo/src/kute.min.js');
